df2d850fd4148f97556e96363f265035c6f0b077
santiagovarela86 <santiagovarela.86@gmail.com>
2018-10-02 23:13:20 -0300

    Se generan los TAJ, TASS y TAS

--- Simulacion_TP6/Program.cs
+++ Simulacion_TP6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -13,6 +14,36 @@ namespace Simulacion_TP6
         [STAThread]
         static void Main()
         {
+            //ESTO LO PUSE PARA PROBAR LA GENERACION DE LOS NUMEROS ALEATORIOS
+            //BORRAR 
+            System.IO.StreamWriter TAJ;
+            TAJ = File.CreateText("C:\\TAJ.txt");
+
+            System.IO.StreamWriter TASS;
+            TASS = File.CreateText("C:\\TASS.txt");
+
+            System.IO.StreamWriter TAS;
+            TAS = File.CreateText("C:\\TAS.txt");
+
+            for (int i = 0; i < 5000; i++)
+            {
+                TAJ.WriteLine(clDatosVar.GenerarTA_J().ToString());   
+            }
+
+            for (int i = 0; i < 5000; i++)
+            {
+                TASS.WriteLine(clDatosVar.GenerarTA_SS().ToString());
+            }
+
+            for (int i = 0; i < 5000; i++)
+            {
+                TAS.WriteLine(clDatosVar.GenerarTA_S().ToString());
+            }
+
+            TAJ.Close();
+            TASS.Close();
+            TAS.Close();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
